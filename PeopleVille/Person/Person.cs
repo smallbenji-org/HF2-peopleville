69f1dac5de13c581906094c40d683446fb0530df
smallbenji <benjaminkfalch@gmail.com>
2026-03-25 12:43:10 +0100

    update person

--- PeopleVille/Person/Person.cs
+++ PeopleVille/Person/Person.cs
@@ -7,12 +7,15 @@ namespace PeopleVille.Persons
     {
         public string Name { get; set; }
         public int Health { get; set; }
-        public List<IEquipment> Inventory { get; set; } = new List<IEquipment>(); // hellere at de starter med tomt inventory end et inventory der ikke eksisterer.
+
+        // hellere at de starter med tomt inventory end et inventory der ikke eksisterer.
+        public List<IEquipment> Inventory { get; set; } = [];
+
         public object Location { get; set; }
         public int Money { get; set; }
         public int Age { get; set; }
         public Location CurrentLocation { get; set; }
-       
+
 
         public void Walk(Location newLocation)
         {
