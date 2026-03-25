0d94ab4ac1705fadc9c5e23e2ad397c0b2e93a3c
aqys <51910@edu.sde.dk>
2026-03-25 10:08:13 +0100

    New WorldBuilder files, and person inventory update

--- PeopleVille/Person/Person.cs
+++ PeopleVille/Person/Person.cs
@@ -6,7 +6,7 @@ namespace PeopleVille.Persons
     {
         public string Name { get; set; }
         public int Health { get; set; }
-        public List<IEquipment> Inventory { get; set; }
+        public List<IEquipment> Inventory { get; set; } = new List<IEquipment>(); // hellere at de starter med tomt inventory end et inventory der ikke eksisterer.
         public object Location { get; set; }
         public int Money { get; set; }
         public int Age { get; set; }
---
+++ PeopleVille/WorldBuilder/WorldBuilder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeopleVille.WorldBuilder
+{
+    internal class WorldBuilder
+    {
+    }
+}
